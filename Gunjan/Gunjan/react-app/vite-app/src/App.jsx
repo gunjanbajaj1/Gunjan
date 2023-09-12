3c6b3c24e553f9ebcc4979cf89774ea8420991d6
gunjanbajaj1 <gunjanbajaj106@gmail.com>
2023-09-12 08:49:11 +0530

    add files

--- Gunjan/Gunjan/react-app/vite-app/src/App.jsx
+++ Gunjan/Gunjan/react-app/vite-app/src/App.jsx
@@ -4,30 +4,37 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [uName, setUName] = useState("");
+  const [uEmail, setUEmail] = useState("");
+  const [uCity, setUCity] = useState("");
 
   return (
     <>
+       <div>
+      <h1>My Profile</h1>
+
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <div className="form-element">
+          <p>Name -{uName}</p>
+          <input type="text" onChange={(e) => setUName(e.target.value)} />
+        </div>
+        <div className="form-element">
+          <p>Your Gender:</p>
+          <input type="radio" name='gender'  onChange={(e) => (e.target.value)}/>
+          <label>Male</label>
+          <input type="radio" name='gender'  onChange={(e) => setUEmail(e.target.value)}/>
+          <label>Female</label>
+        </div>
+        <div className="form-element">
+          <p>City - {uCity}</p>
+          <input
+            type="radio"
+            value={uCity}
+            onChange={(e) => setUCity(e.target.value)}
+          />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    </div>
     </>
   )
 }
